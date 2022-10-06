159e77dc9ec73e237db441a1361f483c77858532
“SenneDmn” <senne.deman@student.ehb.begit config --global user.name SenneDmn git config --global user.email senne.deman@student.ehb.be>
2022-10-06 12:26:40 +0200

    Opdracht 1

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,25 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext('2d');
+
+drawSpaceInvader();
+
+function drawSpaceInvader() {
+
+    context.fillStyle = "#7fd152";
+    context.beginPath();
+    //rechthoek linksboven  
+    context.rect(35, 35, 71, 211);
+    //rechthoek rechtsboven  
+    context.rect(319, 35, 71, 211);
+    //vierkant midden  
+    context.rect(177, 175, 71, 71);
+    //rechthoek midden onder
+    context.rect(106, 246, 213, 71);
+    //rechthoek onder
+    context.rect(35, 317, 355, 71);
+    context.fill();
+}
