a6c64608e4955977bc025abd44ff49b3bbaae3c1
“SenneDmn” <senne.deman@student.ehb.begit config --global user.name SenneDmn git config --global user.email senne.deman@student.ehb.be>
2022-10-06 12:33:24 +0200

    Opdracht 1 deel 2

--- spaceInvader.js
+++ spaceInvader.js
@@ -8,18 +8,22 @@ let canvas = document.querySelector("canvas");
 drawSpaceInvader();
 
 function drawSpaceInvader() {
-
+    //background
+    context.beginPath(); 
+    context.rect(0, 0, 300, 300);
+    context.fill();
+    
     context.fillStyle = "#7fd152";
     context.beginPath();
     //rechthoek linksboven  
-    context.rect(35, 35, 71, 211);
+    context.rect(25, 25, 50, 150);
     //rechthoek rechtsboven  
-    context.rect(319, 35, 71, 211);
+    context.rect(225, 25, 50, 150);
     //vierkant midden  
-    context.rect(177, 175, 71, 71);
+    context.rect(125, 125, 50, 50);
     //rechthoek midden onder
-    context.rect(106, 246, 213, 71);
+    context.rect(75, 175, 150, 50);
     //rechthoek onder
-    context.rect(35, 317, 355, 71);
+    context.rect(25, 225, 250, 50);
     context.fill();
 }
